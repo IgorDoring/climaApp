b7311fb968313bd3033206bc591b744ecc84f0b3
IgorDoring <idoring@tutanota.com>
2024-04-09 02:19:07 -0300

    creating weather component

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Weather } from '../weather';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent {
+  weather: Weather | undefined;
 
+  constructor(public weatherService: WeatherService){
+    
+  }
+
+  search(city: string){
+    this.weatherService.getWeather(city).subscribe(weather => {
+      this.weather = weather;
+    });
+  }
 }
